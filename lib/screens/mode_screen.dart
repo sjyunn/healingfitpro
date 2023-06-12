e89c6b8a70467694439b34be988b64c3b859677a
araseo <sjyunn@gmail.com>
2023-06-12 23:32:10 +0900

    add modechange screen

--- lib/screens/mode_screen.dart
+++ lib/screens/mode_screen.dart
@@ -1,16 +1,76 @@
 import 'package:flutter/material.dart';
 
-class ModeChange extends StatelessWidget {
-  const ModeChange({super.key});
+class ModeChange extends StatefulWidget {
+  const ModeChange({
+    super.key,
+  });
 
+  @override
+  State<ModeChange> createState() => _ModeChangeState();
+}
+
+class _ModeChangeState extends State<ModeChange> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('모드 변경'),
+        centerTitle: true,
       ),
-      body: const Center(
-        child: Text('여기는 힐링핏 모드를 변경하는 곳'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '힐링모드',
+            style: TextStyle(
+              color: Theme.of(context).cardColor,
+              fontSize: 50,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          Text(
+            '힐링모드/학습모드/수면모드',
+            style: TextStyle(
+              color: Theme.of(context).cardColor,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Center(
+            child: Icon(
+              Icons.airline_seat_recline_normal,
+              size: 120,
+              color: Theme.of(context).cardColor,
+            ),
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 25,
+              ),
+              child: Text(
+                '모드 변경',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
